f8549d86f3fcdeb24267b67077bd04f8fe8fb338
ultracold <ultracold273@users.noreply.github.com>
2023-02-28 11:52:45 +0800

    Update index.cshtml.cs to include client certificat

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
 using System.Runtime.InteropServices;
 
 namespace dotnetcoresample.Pages;
@@ -9,11 +10,18 @@ public class IndexModel : PageModel
 
     public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
     
+    public string ClientCertificate { get { return _contextAccessor.HttpContext.Request.Headers["Client-Certificate"]; } }
+    
+    public string ClientCertificateFingerprint { get { return "Fingerprint"; } }
+    
     private readonly ILogger<IndexModel> _logger;
+    
+    private readonly IHttpContextAccessor _contextAccessor;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IHttpContextAccessor contextAccessor)
     {
         _logger = logger;
+        _contextAccessor = contextAccessor;
     }
 
     public void OnGet()
